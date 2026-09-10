5169a03fda020b418fb161711337fe372d43abb3
EC2 Default User <ec2-user@ip-172-31-21-131.ec2.internal>
2026-09-10 23:14:02 +0000

    Final submission

---
+++ hw01-2-summarize-data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "filename,size,num_lines" > wikimedia_data_summary.csv
+
+for file in data/*.csv
+do
+    filename=$(basename "$file")
+    size=$(ls -lh "$file" | awk '{print $5}')
+    num_lines=$(wc -l < "$file" | tr -d ' ')
+    echo "$filename,$size,$num_lines" >> wikimedia_data_summary.csv
+done
